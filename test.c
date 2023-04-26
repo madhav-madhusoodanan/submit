95c75c4d2d41adc260d3f848a070ed2c21f5ce27
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2023-04-26 21:45:33 +0530

    updates

--- test.c
+++ test.c
@@ -1,28 +1,45 @@
-#include<stdio.h> //printf
-#include<string.h> //memset
-#include<stdlib.h> //exit(0);
-#include<unistd.h>
-#include<arpa/inet.h>
-#include<sys/socket.h>
-#include<sys/time.h>
-#include<signal.h>
+#include <stdio.h>
+#include <string.h>
 
-int main(){
-    int *process = malloc(sizeof(pid_t));
-    *process = 0;
-    int pid = fork();
+int main() {
+    char str1[] = "apple,banana,orange";
+    char str2[] = "red,green,blue";
+    char result[1000] = ""; // initialize the result string
 
-    while(1){
-        if(pid == 0){
-            sleep(1);
-            printf(" %d", *process);
-            kill(*process, SIGKILL);
-        } else {
-            *process = pid;
-            printf("Printed!");
-            fflush(stdout);
-            sleep(100);
+    // split the first string into individual words
+    char *token1 = strtok(str1, ",");
+    int i = 1;
+
+    // loop through the words in the first string
+    while (token1 != NULL) {
+        strcat(result, token1); // add the word to the result string
+
+        // if there are more words in the second string, add a comma
+        if (i % 2 == 0) {
+            strcat(result, ",");
         }
+
+        // get the next word from the second string
+        char *token2 = strtok(str2, ",");
+
+        // if there are no more words in the second string, break the loop
+        if (token2 == NULL) {
+            break;
+        }
+
+        strcat(result, token2); // add the word to the result string
+
+        // if there are more words in the first string, add a comma
+        if (token1 = strtok(NULL, ",") != NULL) {
+            strcat(result, ",");
+        }
+
+        // get the next word from the first string
+        token1 = strtok(NULL, ",");
+        i += 2;
     }
-    free(process);
+
+    printf("Result: %s\n", result); // print the result string
+
+    return 0;
 }
